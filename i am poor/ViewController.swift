7dfff66bd24b710e5b0b0bd04723aebff9327c5e
Konstantin <don.kamatoz@bk.ru>
2023-01-20 21:02:16 +0500

    Initial Commit

---
+++ i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Константин Стольников on 2023/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
